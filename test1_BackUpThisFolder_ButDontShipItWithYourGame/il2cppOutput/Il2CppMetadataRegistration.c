cc429193b1c6b609727b249ae96eb47ca256c865
salva-salort <salva.salort@gmail.com>
2026-07-15 13:34:33 +0200

    APK lista

---
+++ test1_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Il2CppMetadataRegistration.c
@@ -0,0 +1,36 @@
+﻿#include "pch-c.h"
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern Il2CppGenericClass* const g_Il2CppGenericTypes[];
+extern const Il2CppGenericInst* const g_Il2CppGenericInstTable[];
+extern const Il2CppGenericMethodFunctionsDefinitions g_Il2CppGenericMethodFunctions[];
+extern const Il2CppType* const  g_Il2CppTypeTable[];
+extern const Il2CppMethodSpec g_Il2CppMethodSpecTable[];
+extern const int32_t* g_FieldOffsetTable[];
+extern const Il2CppTypeDefinitionSizes* g_Il2CppTypeDefinitionSizesTable[];
+IL2CPP_EXTERN_C const Il2CppMetadataRegistration g_MetadataRegistration;
+const Il2CppMetadataRegistration g_MetadataRegistration = 
+{
+	14658,
+	g_Il2CppGenericTypes,
+	11014,
+	g_Il2CppGenericInstTable,
+	149198,
+	g_Il2CppGenericMethodFunctions,
+	44300,
+	g_Il2CppTypeTable,
+	170472,
+	g_Il2CppMethodSpecTable,
+	12451,
+	g_FieldOffsetTable,
+	12451,
+	g_Il2CppTypeDefinitionSizesTable,
+	0,
+	NULL,
+};
